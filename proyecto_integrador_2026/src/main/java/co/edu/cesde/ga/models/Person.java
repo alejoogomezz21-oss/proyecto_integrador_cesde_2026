4e6893395e754ab57ebd2bcb1cde16b338f2ef5d
alejogomez21 <alejogoji777@gmail.com>
2026-02-21 14:00:19 -0500

    Creación de clase person, incluye getter, setters y constructor

---
+++ proyecto_integrador_2026/src/main/java/co/edu/cesde/ga/models/Person.java
@@ -0,0 +1,71 @@
+package co.edu.cesde.ga.models;
+
+public class Person {
+
+    Long  userId;
+    String code;
+    String documentNumber;
+    String firstName;
+    String lastName;
+    Boolean status;
+
+    public Person() {
+    }
+
+    public Person(Boolean status, String firstName, String lastName, String documentNumber, String code, Long userId) {
+        this.status = status;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.documentNumber = documentNumber;
+        this.code = code;
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
