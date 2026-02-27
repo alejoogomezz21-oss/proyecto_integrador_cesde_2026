bf4b2ee75ca67b395eb86fc87b4f5db641041f1f
alejogomez21 <alejogoji777@gmail.com>
2026-02-27 13:51:25 -0500

    Actualización del main para el funcionamiento de los models

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -1,8 +1,20 @@
 package co.edu.cesde.ga.app;
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello word");
-    }
+import co.edu.cesde.ga.models.Person;
+import co.edu.cesde.ga.models.Student;
+import co.edu.cesde.ga.models.Teacher;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Student(true, "Carlos", "Perez", "1010", "S-01", 1L));
+        people.add(new Teacher(true, "Marta", "Ruiz", "2020", "T-01", 2L));
+
+        for (Person p : people) {
+            System.out.println("Registrado: " + p.getFirstName() + " " + p.getLastName());
+        }
+    }
 }
